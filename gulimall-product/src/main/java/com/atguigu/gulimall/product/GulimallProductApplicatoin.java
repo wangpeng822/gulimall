aaac6e026b160da798c17a5f11e1fea6d72be1d8
zhouxingxing <zhouxingxing822@Gmail.com>
2025-11-18 23:52:58 +0800

    feat:新增逻辑删除和JSO303校验 pom文件中额外新增打印日志级别

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplicatoin.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplicatoin.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1.整合Mybatis-plus
@@ -14,8 +15,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *     2).配置Mybatis-plus
  *       1).在启动类添加@MapperScan注解扫描mapper接口
  *       2).告诉Mybatis-plus 映射文件位置
- *
+ * 2.逻辑删除
+ *      配置全局的逻辑删除的规则(高版本可以省略)
+ *      配置逻辑删除的组件Bean(同上)
+ *      给Bean加上逻辑删除注解@TableLogic
+ * 3.JSR303
+ *    1.给Bean添加校验注解
+ *    2.开启校验注解
+ *    3.给校验的bean后紧跟一个BindingResult,就可以获取到校验的结果
+ * 4.统一的异常处理
+ * @ControllerAdvice
+ *  1.
  */
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplicatoin {
